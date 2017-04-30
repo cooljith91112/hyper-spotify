623ff573e2078fcc83a9d775f79fd2f7ac792d4f
Mattia Panzeri <mattia.panzeri93@gmail.com>
2017-04-30 22:33:11 +0200

    Improved TrackInfo component
    Display artist name only if is available

--- src/components/TrackInfo.js
+++ src/components/TrackInfo.js
@@ -1,7 +1,7 @@
 const TrackInfoFactory = (React) => ({ track }) => {
   return (
     <div>
-      <b>{track.name}</b> by <b>{track.artist}</b>
+      <b>{track.name}</b> { track.artist ? <span> by <b>{track.artist}</b></span> : '' }
     </div>
   )
 }
