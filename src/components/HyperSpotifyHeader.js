7a9bb9ce1add09ba1dcde8fd447dc29cb8d6c629
Mattia Panzeri <mattia.panzeri93@gmail.com>
2017-06-06 23:47:36 +0200

    Add `Positioning` feature

---
+++ src/components/HyperSpotifyHeader.js
@@ -0,0 +1,35 @@
+import Radium from 'radium'
+import HyperSpotifyWidgetFactory from '../containers/HyperSpotifyWidget'
+
+export const HyperSpotifyHeaderFactory = (React) => {
+  const HyperSpotifyWidget = HyperSpotifyWidgetFactory(React) // eslint-disable-line no-unused-vars
+
+  return Radium(() => (
+    <header style={styles.headerStyle}>
+      <span style={styles.headerOverlayStyle} />
+      <HyperSpotifyWidget />
+    </header>
+  ))
+}
+
+const styles = {
+  'headerStyle': {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'relative',
+    opacity: '0.5',
+    ':hover': {
+      opacity: '1'
+    }
+  },
+  'headerOverlayStyle': {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0,
+    backgroundColor: '#000',
+    opacity: 0.1
+  }
+}
