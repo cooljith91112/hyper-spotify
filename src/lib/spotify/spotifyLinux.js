c9e5016c28f1b0d8e001ad84666424cad47afb04
Mattia Panzeri <mattia.panzeri93@gmail.com>
2017-08-05 12:00:07 +0200

    feat(Linux Support): Finalized Linux support

--- src/lib/spotify/spotifyLinux.js
+++ src/lib/spotify/spotifyLinux.js
@@ -24,6 +24,26 @@ class SpotifyLinux {
       .then(spotify => this._getMetadata(spotify))
   }
 
+  togglePlayPause () {
+    return this._getSpotifyInterface()
+      .then(spotify => this._playPause(spotify))
+      .then(spotify => this._getPlaybackStatus(spotify))
+      .then(status => Promise.resolve({ state: toLower(status) }))
+  }
+
+  previousTrack () {
+    return this._getSpotifyInterface()
+      .then(spotify => this._previous(spotify))
+      .then(spotify => this._getMetadata(spotify))
+  }
+
+  nextTrack () {
+    return this._getSpotifyInterface()
+      .then(spotify => this._next(spotify))
+      .then(spotify => this._getMetadata(spotify))
+  }
+
+  // Internal methods
   _getSpotifyInterface () {
     return new Promise((resolve, reject) => {
       this.spotifyService.getInterface('/org/mpris/MediaPlayer2', 'org.mpris.MediaPlayer2.Player', function (err, spotify) {
@@ -66,6 +86,43 @@ class SpotifyLinux {
           resolve(m)
         }
       })
+    })
+  }
+
+  _playPause (spotifyInterface) {
+    return new Promise(function (resolve, reject) {
+      try {
+        spotifyInterface.PlayPause(function () {
+          setTimeout(() => resolve(spotifyInterface), 500)
+        })
+      } catch (err) {
+        reject(err)
+      }
+    })
+  }
+
+  _next (spotifyInterface) {
+    return new Promise(function (resolve, reject) {
+      try {
+        spotifyInterface.Next(function () {
+          console.log('next args', arguments)
+          setTimeout(() => resolve(spotifyInterface), 500)
+        })
+      } catch (err) {
+        reject(err)
+      }
+    })
+  }
+
+  _previous (spotifyInterface) {
+    return new Promise(function (resolve, reject) {
+      try {
+        spotifyInterface.Previous(function () {
+          setTimeout(() => resolve(spotifyInterface), 500)
+        })
+      } catch (err) {
+        reject(err)
+      }
     })
   }
 }
